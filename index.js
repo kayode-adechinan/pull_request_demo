dc502266c80e09bb543c0e888a6bf69f4b467222
Kayode Adechinan <kayode.adechinan@gmail.com>
2019-03-16 18:37:02 -0400

    it hurts

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app  = express()
 
 // home entry point
-app.get('/', (req, res)=> res.send('better hello from proposal2'))
+app.get('/', (req, res)=> res.send('it hurts'))
 
 app.listen(3000, '0.0.0.0', () => console.log('it works'))
