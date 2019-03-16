7df0a320cd1b39bde94c98660887b7ba0a88ba58
trazalcar <kayode.adechinan@yahoo.com>
2019-03-16 19:52:14 +0000

    adding proposal

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app  = express()
 
 // home entry point
-app.get('/', (req, res)=> res.send('hello'))
+app.get('/', (req, res)=> res.send('better hello'))
 
 app.listen(3000, '0.0.0.0', () => console.log('it works'))
