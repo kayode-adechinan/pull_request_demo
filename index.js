46bc7673a6abf4a3873fb1c31fb3a0b5b01d83e2
trazalcar <kayode.adechinan@yahoo.com>
2019-03-16 22:45:35 +0000

    making correction effective

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app  = express()
 
 // home entry point
-app.get('/', (req, res)=> res.send('patience works'))
+app.get('/', (req, res)=> res.send('courage'))
 
 app.listen(3000, '0.0.0.0', () => console.log('it works'))
